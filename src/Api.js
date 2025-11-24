a922ff6e95af4518e7a1ce73fff9074f363fe2c6
KRISHNARAJENDRASAGAR <kris@KRISHNARAJENDRASAGARs-MacBook-Air.local>
2025-11-24 20:43:14 +0530

    Api

--- src/Api.js
+++ src/Api.js
@@ -1,43 +1,45 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5215/api/items";
-const Auth_URL = "http://localhost:5215/api/auth/";
+const API_BASE = process.env.REACT_APP_API_BASE;
+
+const BASE_URL = `${API_BASE}/api/items`;
+const AUTH_URL = `${API_BASE}/api/auth`;
 
 export async function getItems() {
     const response = await axios.get(BASE_URL);
     return response.data;
-};
+}
 
 export const createItem = async (item) => {
     const response = await axios.post(BASE_URL, item);
     return response.data;
 };
 
-export const deleteItem = async(id) => {
+export const deleteItem = async (id) => {
     const response = await axios.delete(`${BASE_URL}/${id}`);
     return response.data;
 };
 
-export const updateItem = async(id, item) => {
+export const updateItem = async (id, item) => {
     const response = await axios.put(`${BASE_URL}/${id}`, item);
     return response.data;
-}
+};
 
-export const signUp = async(Email, Password) => {
-    const response = await axios.post(`${Auth_URL}signup`, { Email, Password });
+export const signUp = async (Email, Password) => {
+    const response = await axios.post(`${AUTH_URL}/signup`, { Email, Password });
     return response.data;
-}
+};
 
-export const login = async(Email, Password) => {
-    const response = await axios.post(`${Auth_URL}login`, { Email, Password });
+export const login = async (Email, Password) => {
+    const response = await axios.post(`${AUTH_URL}/login`, { Email, Password });
     return response.data;
-}
+};
 
-export const me = async(token) => {
-    const response = await axios.get(`${Auth_URL}me`, {
-        headers : {
-            Authorization : `Bearer ${token}`,
+export const me = async (token) => {
+    const response = await axios.get(`${AUTH_URL}/me`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
         },
     });
     return response.data;
-}
+};
